39692004e20552f1d43e40357e0cc73117065a5e
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-01-10 17:45:23 -0300

    Edit name class

--- src/main/java/br/com/microservice/transportador/controller/DeliveryController.java
+++ src/main/java/br/com/microservice/transportador/controller/DeliveryController.java
@@ -1,9 +1,9 @@
 package br.com.microservice.transportador.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.microservice.transportador.dto.DeliveryDTO;
@@ -17,8 +17,8 @@ public class DeliveryController {
 	@Autowired
 	private IDeliveryService deliveryService;
 
-	@RequestMapping(method = RequestMethod.POST)
-	public VoucherDTO reservaEntrega(@RequestBody DeliveryDTO pedidoDTO) {
-		return deliveryService.bookDelivery(pedidoDTO);
+	@PostMapping
+	public VoucherDTO bookDelivery(@RequestBody DeliveryDTO deliveryDTO) {
+		return deliveryService.bookDelivery(deliveryDTO);
 	}
 }
